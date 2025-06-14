bda607d5d3c529530b3763831aae5ca961bc3a34
OneNull <shavrin_ns@mail.ru>
2025-06-14 14:12:52 +0300

    Road unit test realization

--- src/test/java/ru/vsu/cd/oop/generator/logic/map/elements/RoadTest.java
+++ src/test/java/ru/vsu/cd/oop/generator/logic/map/elements/RoadTest.java
@@ -1,5 +1,23 @@
 package ru.vsu.cd.oop.generator.logic.map.elements;
 
-public class RoadTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ru.vsu.cs.oop.generator.logic.map.elements.*;
+
+
+class RoadTest {
+
+    private Location a = new Location(0, "Ноль");
+    private Location b = new Location(1, "Один");
+
     
+    @Test
+    void testConstructorAndGetters() {
+        Road road = new Road(a, b);
+
+        assertEquals(a, road.getStart());
+        assertEquals(b, road.getEnd());
+    }
+
 }
