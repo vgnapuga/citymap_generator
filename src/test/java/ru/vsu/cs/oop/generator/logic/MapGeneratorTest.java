a3f094f2c98a161d4edb7adffb55fcd761ffa03f
OneNull <shavrin_ns@mail.ru>
2025-06-14 14:41:56 +0300

    MapGenerator unit test realization

--- src/test/java/ru/vsu/cs/oop/generator/logic/MapGeneratorTest.java
+++ src/test/java/ru/vsu/cs/oop/generator/logic/MapGeneratorTest.java
@@ -1,5 +1,27 @@
 package ru.vsu.cs.oop.generator.logic;
 
-public class MapGeneratorTest {
+import org.junit.jupiter.api.Test;
+import ru.vsu.cs.oop.generator.logic.map.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MapGeneratorTest {
     
+    @Test
+    void testGenerateValidMap() {
+        CityMap map = MapGenerator.generate(5, 2);
+        assertNotNull(map);
+        assertEquals(5, map.getLocations().size());
+        assertFalse(map.getRoads().isEmpty());
+    }
+    
+    @Test
+    void testInvalidParameters() {
+        assertThrows(IllegalArgumentException.class,
+            () -> MapGenerator.generate(0, 2));
+        
+        assertThrows(IllegalArgumentException.class,
+            () -> MapGenerator.generate(5, 0));
+    }
+
 }
